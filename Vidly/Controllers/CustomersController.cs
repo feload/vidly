b04d51352dc330381fc3bf56839fc128e1ef8fed
dellirium <feload@gmail.com>
2017-03-05 19:35:41 -0600

    Inicial commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers;
+        public CustomersController()
+        {
+            _customers = new List<Customer>()
+            {
+                new Customer() { Id = 1, Name = "Juan" },
+                new Customer() { Id = 2, Name = "Petra" },
+            };
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            return View(_customers.Find(c => c.Id == Id));
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>()
+            {
+                new Movie() { Name = "Shark!" },
+                new Movie() { Name = "Tank!" },
+                new Movie() { Name = "Barracuda!" },
+            };
+
+            return View(movies);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/(month:regex(\\d{2}):range(1, 12))")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(String.Format("{0} {1}", year, month));
+        }
+    }
+}
