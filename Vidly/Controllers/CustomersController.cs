98188c2edb9741450580b9551330b92d9fba0ec5
dellirium <dellirium>
2017-03-13 21:02:11 -0700

    Add antiforgery token

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -40,6 +40,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(CustomerFormViewModel viewModel)
         {
             var customer = viewModel.Customer;
